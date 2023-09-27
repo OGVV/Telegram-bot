65bbd8c0182bd169b3d1e85f64997432e213fa67
Vladimir <djeksaly@gmail.com>
2023-09-27 06:26:27 +0300

    TelegBot_2-Add telegram bot

--- src/main/java/com/github/Telegrambot/TestTelegramBot/TelegramBotApplication.java
+++ src/main/java/com/github/Telegrambot/TestTelegramBot/TelegramBotApplication.java
@@ -1,4 +1,4 @@
-package com.github.Telegrambot.TestTelegramBot.jrtb;
+package com.github.Telegrambot.TestTelegramBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/github/Telegrambot/TestTelegramBot/service/TelegramBotApplicationTests.java
+++ src/test/java/com/github/Telegrambot/TestTelegramBot/service/TelegramBotApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.Telegrambot.TestTelegramBot.jrtb;
+package com.github.Telegrambot.TestTelegramBot.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
